c11968bf2dbed90cd9a36da48a63afcefe4d7c65
max-student@outlook.com <max-student@outlook.com>
2019-08-19 10:55:46 -0400

    Add project files.

---
+++ MyWebServer/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MyWebServer {
+    class Program {
+        static void Main(string[] args) {
+            HttpListener listener = new HttpListener();
+            string uri = "http://localhost:8080/";
+            listener.Prefixes.Add(uri);
+            listener.Start();
+
+            //loop and wait for the next request.
+            while(true) {
+                Console.WriteLine("\nListening on " + uri + "...");
+                // wait for a requet
+                HttpListenerContext context = listener.GetContext();
+                Console.WriteLine("\nIncoming request received!");
+
+                //Get the incoming request
+                HttpListenerRequest request = context.Request;
+
+                string url = request.Url.ToString();
+                Console.WriteLine("URL: " + url);
+                Console.WriteLine("Method: " + request.HttpMethod);
+
+                //Send a response to the browser
+                HttpListenerResponse response = context.Response;
+                string responseString = "Hello World!";
+
+
+            }
+
+
+        }
+    }
+}
